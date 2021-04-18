3a71a50b99dfb5a8a8af117d310e0f7b68854139
Kikibyk <57362643+Kikibyk@users.noreply.github.com>
2021-04-18 23:00:10 +0300

    Увеличил скорость в 5 раз!!!

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-const { VK } = require('vk-io');
 const config = require('./config.json');
-const vk = new VK({token: config.token});
+const vk = new (require('vk-io')).VK({token: config.token});
 let i = 0;
 
-async function Main() {
-    if(i > 9000) return; // Установлено ограничение, чтобы не испытывать проблемы с загрузкой скринов на потряжении следующего дня (загружать можно 10к фотографий в день)
-    await vk.upload.photoAlbum({source: {value: './photo.jpg'}, album_id: config.album_id}); // Загружаем фотографию в альбом
-    i++;
+async function go() {
+    if(i > 9000) return clearInterval(); // Установлено ограничение, чтобы не испытывать проблемы с загрузкой скринов на потряжении следующего дня (загружать можно 10к фотографий в день)
+    await vk.upload.photoAlbum({source: ['./photo.jpg', './photo.jpg', './photo.jpg', './photo.jpg', './photo.jpg'], values: {value: './photo.jpg', filename: 'photo', contentType: 'image'}, album_id: config.album_id});i+=5; // Загружаем фотографию в альбом
     console.log(`\x1b[32m> \x1b[0mЗагружено фотографий: ${i}`); // Выводим в консоль количество загруженных фотографий
-    setTimeout(Main, 50); // Повторяем функцию через 50 миллисекунд, дабы не было ошибки Flood Control
-}Main()
+    setTimeout(go, 250); // Повторяем функцию через 250 миллисекунд, дабы не было Flood Control
+}go();
