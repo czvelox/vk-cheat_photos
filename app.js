65a10e7d27d3b4e06a86bc05b34782a8fb822fe6
Kikibyk <57362643+Kikibyk@users.noreply.github.com>
2021-06-21 14:14:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let i = 0;
 
 async function go() {
     if(i >= 9000) return clearInterval(); // Установлено ограничение, чтобы не испытывать проблемы с загрузкой скринов на потряжении следующего дня (загружать можно 10к фотографий в день)
-    await vk.upload.photoAlbum({source: ['./photo.jpg', './photo.jpg', './photo.jpg', './photo.jpg', './photo.jpg'], values: {value: './photo.jpg', filename: 'photo', contentType: 'image'}, album_id: config.album_id});i+=5; // Загружаем фотографию в альбом
+    await vk.upload.photoAlbum({source: ['./photo.jpg', './photo.jpg', './photo.jpg', './photo.jpg', './photo.jpg'], values: {value: './photo.jpg', filename: 'photo', contentType: 'image'}, album_id: require('./config.json').album_id});i+=5; // Загружаем фотографию в альбом
     console.log(`\x1b[32m> \x1b[0mЗагружено фотографий: ${i}`); // Выводим в консоль количество загруженных фотографий
     setTimeout(go, 250); // Повторяем функцию через 250 миллисекунд, дабы не было Flood Control
 }go();
