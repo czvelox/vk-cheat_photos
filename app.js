e109ba02d1ef67ab4873a55406172b5cf7cb453f
Илья <57362643+Kikibyk@users.noreply.github.com>
2021-10-12 01:25:37 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const config = require('./config.json');
-const vk = new (require('vk-io')).VK({token: config.token});
-let i = 0;
+const { token, album_id } = require('./config.json');
+const { upload } = new (require('vk-io')).VK({token});
 
-async function go() {
-    if(i > 9000) return clearInterval(); // Установлено ограничение, чтобы не испытывать проблемы с загрузкой скринов на потряжении следующего дня (загружать можно 10к фотографий в день)
-    await vk.upload.photoAlbum({source: ['./photo.jpg', './photo.jpg', './photo.jpg', './photo.jpg', './photo.jpg'], values: {value: './photo.jpg', filename: 'photo', contentType: 'image'}, album_id: config.album_id});i+=5; // Загружаем фотографию в альбом
-    console.log(`\x1b[32m> \x1b[0mЗагружено фотографий: ${i}`); // Выводим в консоль количество загруженных фотографий
-    setTimeout(go, 250); // Повторяем функцию через 250 миллисекунд, дабы не было Flood Control
-}go();
+(async () => {
+    for(let i = 0; i < 9000; i += 5) {
+        console.log(`\x1b[32m> \x1b[0mЗагружено фотографий: ${i}`);
+
+        await upload.photoAlbum({source: Array(5).fill('./photo.jpg'), album_id});
+        await new Promise(resolve => setTimeout(resolve, 250));
+    }
+})();
